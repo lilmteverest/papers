b177de415e77e565dcf1951806947f4ab5ec7031
lilmteverest <94333203+lilmteverest@users.noreply.github.com>
2021-11-14 21:37:41 -0500

    Update monte_carlo.py

--- options-pricing/monte_carlo.py
+++ options-pricing/monte_carlo.py
@@ -15,27 +15,27 @@ np.random.seed(1)
 def sample(num):
     return np.random.normal(0, 1, num)
 
-def calculateAssetPrices(sampleSet,S_t,t,T,r,sigma):
-    assetPrices = []
-    for epsilon in sampleSet:
-       assetPrice = S_t * math.e ** ((r - (sigma**2)/2)*(T-t) + epsilon*sigma*math.sqrt(T-t))
-       assetPrices.append(assetPrice)
-    return assetPrices
+def calculate_asset_prices(sample_set,S_t,t,T,r,sigma):
+    asset_prices = []
+    for epsilon in sample_set:
+       asset_price = S_t * math.e ** ((r - (sigma**2)/2)*(T-t) + epsilon*sigma*math.sqrt(T-t))
+       asset_prices.append(asset_price)
+    return asset_prices
 
-#print(calculateAssetPrices(sampleSet,174.67,0,5,.0118,.1786))
+#print(calculate_asset_prices(sample_set,174.67,0,5,.0118,.1786))
 
-def calculateOptionPrices(assetPrices,t,T,r,K): #K is the strike price
-    optionPrices = []
-    for assetPrice in assetPrices:
-        optionPrice = (math.e ** (-r*(T-t))) * max(0,assetPrice - K)
-        optionPrices.append(optionPrice)
-    return optionPrices
+def calculate_option_prices(asset_prices,t,T,r,K): #K is the strike price
+    option_prices = []
+    for asset_price in asset_prices:
+        option_price = (math.e ** (-r*(T-t))) * max(0,assetPrice - K)
+        option_prices.append(option_price)
+    return option_prices
 
-def meanOptionPrice(num,S_t,t,T,r,sigma,K):
-    sampleSet = sample(num)
-    assetPrices = calculateAssetPrices(sampleSet,S_t,t,T,r,sigma)
-    optionPrices = calculateOptionPrices(assetPrices,t,T,r,K)
-    return sum(optionPrices)/len(optionPrices)
+def mean_option_price(num,S_t,t,T,r,sigma,K):
+    sample_set = sample(num)
+    asset_prices = calculate_asset_prices(sampleSet,S_t,t,T,r,sigma)
+    option_prices = calculate_option_prices(assetPrices,t,T,r,K)
+    return sum(option_prices)/len(option_prices)
 
-print(meanOptionPrice(100,.,0,5,.0118,.1786,170))
+print(mean_option_price(100,.,0,5,.0118,.1786,170))
                         
